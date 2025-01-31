31ef4b317a24fbef352085f115dee54883322143
Adam Simon <adamsimon@sweetpunk.com>
2025-01-31 15:56:10 +0100

    added style to upload page

--- client/src/app/upload/page.tsx
+++ client/src/app/upload/page.tsx
@@ -2,9 +2,11 @@ import UploadForm from "@/components/llm/upload";
 
 const UploadPage = () => {
   return (
-    <div>
-      <h1>Upload File</h1>
-      <UploadForm />
+    <div className="flex min-h-screen items-center justify-center bg-black p-6">
+      <div className="w-full max-w-lg bg-white shadow-xl rounded-2xl p-8">
+        <h1 className="text-3xl font-bold text-custom-blue text-center mb-6">Upload File</h1>
+        <UploadForm />
+      </div>
     </div>
   );
 };

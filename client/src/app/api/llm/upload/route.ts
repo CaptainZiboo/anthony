6e85070da763ff9085297f99a48e011839625d1e
CaptainZiboo <96137708+CaptainZiboo@users.noreply.github.com>
2025-01-31 09:52:29 +0100

    fix: file import + processing

--- client/src/app/api/llm/upload/route.ts
+++ client/src/app/api/llm/upload/route.ts
@@ -2,7 +2,13 @@ import * as uuid from "uuid";
 import fs from "fs";
 import path from "path";
 import { TextLoader } from "langchain/document_loaders/fs/text";
-import { CharacterTextSplitter } from "langchain/text_splitter";
+import {
+  CharacterTextSplitter,
+  RecursiveCharacterTextSplitter,
+} from "langchain/text_splitter";
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { RedisVectorStore } from "@langchain/redis";
+import { createClient } from "redis";
 
 export const config = {
   api: {
@@ -32,27 +38,63 @@ export async function POST(request: Request) {
         // Save the file temporarily
         fs.writeFileSync(_path, buffer);
 
-        // Load the document using TextLoader
-        const loader = new TextLoader(_path);
-        const docs = await loader.load();
+        try {
+          // Load the document using TextLoader
+          const loader = new TextLoader(_path);
+          const docs = await loader.load();
 
-        // Split the document into characters
-        const splitter = new CharacterTextSplitter({
-          chunkSize: 100,
-          chunkOverlap: 0,
-        });
-        const texts = await splitter.splitDocuments(docs);
+          // Split the document into characters
+          const splitter = new CharacterTextSplitter({
+            chunkSize: 100,
+            chunkOverlap: 0,
+          });
+          const texts = await splitter.splitDocuments(docs);
 
-        console.log(texts);
+          const recursive_splitter = new RecursiveCharacterTextSplitter();
 
-        // Collect metadata
-        metadataList.push({
-          docs,
-          texts,
-        });
+          const recursive_texts = await recursive_splitter.splitDocuments(
+            texts
+          );
 
-        // Delete the file after processing
-        fs.unlinkSync(_path);
+          // if file is HTML, Markdown or JSON
+          const documentSplitter =
+            RecursiveCharacterTextSplitter.getSeparatorsForLanguage("js");
+
+          console.log("Generated recursive_texts");
+
+          const embeddings = new OllamaEmbeddings({
+            model: "mxbai-embed-large",
+            baseUrl: "http://localhost:11434",
+          });
+
+          const client = createClient({
+            url: process.env.REDIS_URL ?? "redis://localhost:6379",
+          });
+
+          await client.connect();
+
+          console.log("Connected to Redis");
+
+          const store = new RedisVectorStore(embeddings, {
+            redisClient: client,
+            indexName: "ollama",
+          });
+
+          await store.addDocuments(recursive_texts);
+
+          console.log("Stored documents in Redis");
+
+          // Collect metadata
+          metadataList.push({
+            docs,
+            recursive_texts,
+          });
+        } catch (error) {
+          throw error;
+        } finally {
+          // Delete the file after processing
+          fs.unlinkSync(_path);
+        }
       }
     }
 
